5077754ae99f2cec504b2c29a31d698d51f53b6a
Karim Hassan <karim.hassan@linkdev.com>
2024-10-13 16:59:20 +0300

    update create order

--- Modules/Client/Services/OrderService.php
+++ Modules/Client/Services/OrderService.php
@@ -73,15 +73,17 @@ class OrderService
             $products_price += $total;
         }
 
+        $user_car = UserCar::where('car_id', $request->input('user_car_id'))->where('user_id', $this->user->id)->first();
+
         $order_data = [
-            'user_car_id' => $request->input('user_car_id'),
+            'user_car_id' => $user_car?->id,
             'address_id' => $request->input('address_id'),
             'delivery_time' =>  $delivery_time,
             'payment_method' => $request->input('payment_method'),
             'user_id' => $this->user->id,
             'status' => 'new',
             'type' =>  $type,
-            'car_id' => UserCar::find($request->input('user_car_id'))->car_id,
+            'car_id' => $request->input('user_car_id'),
             'products_price' => $products_price,
             'services_price' => 0,
             'tax_price' => 0,
